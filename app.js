d1252b97b20b539d4a881b782f441c6df12e5e24
Paul-O-95 <pauleke95@gmail.com>
2020-02-21 12:24:16 +0100

    Fixed app.js

--- app.js
+++ app.js
@@ -1,47 +1,46 @@
-(function(){
-const loverName =document.getElementsByClassName('lover-name');
-const loverText = document.getElementsByClassName('lover-text');
-const buttons = document.querySelectorAll('a')
-let index = 0;
-const lovers = []
-console.log(buttons)
+(function () {
+    const loverName = document.getElementsByClassName('lover-name');
+    const loverText = document.getElementsByClassName('lover-text');
+    const buttons = document.querySelectorAll('a')
+    let index = 0;
+    const lovers = []
+    // console.log(buttons)
 
-// Lover Constructor
-function Lover (name,text){
-    this.name = name;
-    this.text = text
-}
+    // Lover Constructor
+    function Lover(name, text) {
+        this.name = name;
+        this.text = text
+    }
 
-//Creates new Lover
-function createLover(name,text){
-    let lover = new Lover (name,text)
-    lovers.push(lover)
-}
+    //Creates new Lover
+    function createLover(name, text) {
+        let lover = new Lover(name, text)
+        lovers.push(lover)
+    }
 
-createLover('Folasayo',"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo qui quidem sit quisquam eveniet accusamus ducimus! Fugit laboriosam nihil eaque.");
-createLover('Oluwaseyi',"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo qui quidem sit quisquam eveniet accusamus ducimus! Fugit laboriosam nihil eaque.");
-createLover('Fikunayomi',"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo qui quidem sit quisquam eveniet accusamus ducimus! Fugit laboriosam nihil eaque.");
+    createLover('Folasayo', "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo qui quidem sit quisquam eveniet accusamus ducimus! Fugit laboriosam nihil eaque.");
+    createLover('Oluwaseyi', "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo qui quidem sit quisquam eveniet accusamus ducimus! Fugit laboriosam nihil eaque.");
+    createLover('Fikunayomi', "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illo qui quidem sit quisquam eveniet accusamus ducimus! Fugit laboriosam nihil eaque.");
 
-buttons.forEach(function(button){
-    button.addEventListener('click', function(e){
-        if (e.target.parentElement.classList.contains('prevBtn')){
-           if(index === 0){
-                index = lovers.length
-           }
-           console.log(index)
-           index--
-           loverName.textContent = lovers[index].name
-           loverText.textContent = lovers[index].text
-        }
-        if (e.target.parentElement.classList.contains('nextBtn')){
-            index++
-            if(index === lovers.length){
-                 index = 0
+    buttons.forEach(function (button) {
+        button.addEventListener('click', function (e) {
+            if (e.target.classList.contains('prevBtn')) {
+                if (index === 0) {
+                    index = lovers.length;
+                }
+                index--;
+                loverName[0].textContent = lovers[index].name;
+                loverText[0].textContent = lovers[index].text;
             }
-            loverName.textContent = lovers[index].name
-            loverText.textContent = lovers[index].text
-         }
-    })
-})
-})()
+            if (e.target.classList.contains('nextBtn')) {
+                index++;
+                if (index === lovers.length) {
+                    index = 0;
+                }
+                loverName[0].textContent = lovers[index].name;
+                loverText[0].textContent = lovers[index].text;
+            }
+        });
+    });
+})();
 
